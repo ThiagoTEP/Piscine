68e1d4429538b47d767a861257a1a62e60256156
ThiagoTEP <thiagodepaulapt@gmail.com>
2025-03-26 21:11:22 +0000

    pointers

---
+++ C01/ex00/ft_ft.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+void ft_ft(int *nbr)
+{
+	*nbr = 42;
+}
+/*
+#include <stdio.h>
+int main(void)
+{
+	int x;
+	int *nbr = &x;
+	ft_ft(nbr);
+	printf("%d", x);
+	return 0;
+}
+*/
